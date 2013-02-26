d19cfaf0fcf9140fc369bb257ad1737ab4005a63
Wael M. Nasreddine <wnasreddine@google.com>
2013-02-25 17:08:52 -0800

    building ui

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
